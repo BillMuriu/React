6cef58ecfe6d8d6e246c3bb37d385c5e3c19db1a
unknown <njorogekamau63@gmail.com>
2024-01-18 21:13:12 +0300

    styling components in react

--- pedrotech/ep1/src/App.js
+++ pedrotech/ep1/src/App.js
@@ -2,23 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <User name="Bill Kamau" age={26} email="bill@kamau.com"/>
-      <User name="Eden Ngugo" age={34} email="edden@kamau.com"/>
-      <User name="Bill Kamau" age={26} email="bill@kamau.com"/>
-    </>
-  );
-}
-
-
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
+    <div className='App'>
+      <h1 className='name'>Bill</h1>
     </div>
-  )
+  );
 }
 
 export default App;
