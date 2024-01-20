e3a3f23f3eab776e5ba593a0160db2d68f62e400
unknown <njorogekamau63@gmail.com>
2024-01-20 13:58:36 +0300

    Working with APIs

--- pedrotech/ep1/src/App.js
+++ pedrotech/ep1/src/App.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import "./App.css"
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 const App = () => {
 
-  const [catFact, setCatFact] = useState("")
-
-  const changeFact = () => {
-    fetch('https://catfact.ninja/fact')
-      .then((res) => res.json())
-      .then((data) => {
-        setCatFact(data.fact);
+  const [theexcuse, setTheexcuse] = useState('');
+  
+  const generateExcuse = (theme) => {
+    fetch(`https://excuser-three.vercel.app/v1/excuse/${theme}`)
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        const firstObject = data[0]
+        const excuse = firstObject ? firstObject.excuse : "No excuse found"
+        setTheexcuse(excuse)
       })
   }
 
-  useEffect(() => {
-    changeFact();
-  }, [])
-
   return (
     <div className="App">
-      <button onClick={changeFact}> Generate Cat Fact</button>
-      <p>{catFact}</p>
+      <h1> Generate An Excuse </h1>
+
+      <button onClick={() => generateExcuse('party')}>party</button>
+      <button onClick={() => generateExcuse('family')}>Family</button>
+      <button onClick={() => generateExcuse('office')}>Office</button>
+
+
+      <p>{theexcuse}</p>
     </div>
   )
 }
